f52615c2e1d0b561e6e8a00630a2889283a5c2af
key <im_key@hotmail.com>
2019-01-31 02:19:00 +0800

    add header

--- src/main.js
+++ src/main.js
@@ -4,8 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
-import './assets/styles/reset.css'
-import './assets/styles/border.css'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
+/*
+如果同一个url使用次数很多，可以在webpack.base.conf.js修改
+配置项，修改resolve可以对常用的路径修改别名，比方说
+src/assets/styles这个路径经常用，就可以对这个路径进行别名操作
+简写成styles就可以了
+*/
 
 Vue.config.productionTip = false
 fastclick.attach(document.body)
---
+++ src/pages/home/components/header.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+        <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+        <span class="iconfont">&#xe632;</span>
+        输入城市/景点/主题
+    </div>
+    <div class="header-right">
+        城市
+        <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+/*通过@import '文件路径'就可以引用文件,之后我们就可以使用$bgColor这个变量*/
+/*如果是引用src里面的文件，直接 @/文件路径 就可以了，*/
+/*但是因为是样式文件引用，所以要写成 ~@ 这个形式*/
+@import '~styles/varibles.styl'
+  .header
+    background: $bgColor
+    display: flex
+    line-height: .86rem
+    color: #fff
+    .header-left
+      width: .64rem
+      float: left
+      .back-icon
+        text-align: center
+        font-size: .4rem
+    .header-input
+      flex: 1
+      margin-top: .12rem
+      margin-left: .2rem
+      padding-left: .2rem
+      height: .64rem
+      line-height: .64rem
+      background: #fff
+      border-radius:.1rem
+      color: #ccc
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align: center
+      .arrow-icon
+        margin-left: -0.04rem
+        font-size: .24rem
+</style>
