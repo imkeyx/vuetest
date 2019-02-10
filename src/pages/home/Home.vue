a769c49180e732db22d7d82b4acd0afe039e70e3
key <im_key@hotmail.com>
2019-02-11 01:55:56 +0800

    finish pages

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -15,6 +15,7 @@ import HomeIcons from './components/Icons.vue'
 import HomeRecommend from './components/Recommend.vue'
 import HomeWeekend from './components/Weekend.vue'
 import axios from 'axios'
+import { mapState } from 'vuex'
 export default {
   name: 'Home',
   components: {
@@ -26,15 +27,19 @@ export default {
   },
   data () {
     return {
+      lasteCity: '',
       swiperList: [],
       iconList: [],
       recommendList: [],
       weekendList: []
     }
   },
+  computed: {
+    ...mapState(['city'])
+  },
   methods: {
     getHomeInfo () {
-      axios.get('/api/index.json').then(this.getHomeInfoSucc)
+      axios.get('/api/index.json?city=' + this.city).then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc (res) {
       res = res.data
@@ -48,7 +53,14 @@ export default {
     }
   },
   mounted: function () {
+    this.lasteCity = this.city
     this.getHomeInfo()
+  },
+  activated () {
+    if (this.lasteCity !== this.city) {
+      this.lasteCity = this.city
+      this.getHomeInfo()
+    }
   }
 }
 </script>
