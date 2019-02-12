166c5ca51de3e307188b5f082264783e625426f7
key <im_key@hotmail.com>
2019-02-12 20:14:50 +0800

    ajax

--- src/router/index.js
+++ src/router/index.js
@@ -20,5 +20,8 @@ export default new Router({
       path: '/detail/:id',
       name: 'Detail',
       component: Detail
-    }]
+    }],
+  scrollBehavior (to, from, savedposition) {
+    return {x: 0, y: 0}
+  }
 })
