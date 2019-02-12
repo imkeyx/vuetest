56adb482cf8dfcc73d0d260ee5679b6a97234696
key <im_key@hotmail.com>
2019-02-12 22:30:36 +0800

    finish project

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/pages/home/home.vue'
-import City from '@/pages/city/City.vue'
-import Detail from '@/pages/detail/Detail.vue'
 
 Vue.use(Router)
 
@@ -11,15 +8,15 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: Home
+      component: () => import('@/pages/home/home.vue')
     }, {
       path: '/city',
       name: 'City',
-      component: City
+      component: () => import('@/pages/city/City.vue')
     }, {
       path: '/detail/:id',
       name: 'Detail',
-      component: Detail
+      component: () => import('@/pages/detail/Detail.vue')
     }],
   scrollBehavior (to, from, savedposition) {
     return {x: 0, y: 0}
